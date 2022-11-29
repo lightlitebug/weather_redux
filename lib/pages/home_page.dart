620695e64fecd4c98bc3eb6e4e50270d2b31ef37
lightlitebug <light.lite.bug@gmail.com>
2022-11-29 16:52:12 +0900

    SearchPage

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'search_page.dart';
+
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -8,11 +10,27 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  String? _city;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Weather'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.search),
+            onPressed: () async {
+              _city = await Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) {
+                  return const SearchPage();
+                }),
+              );
+              print('city: $_city');
+            },
+          ),
+        ],
       ),
       body: const Center(
         child: Text('Home'),
