d9bd24c9a100b7d52803a8f589445403baf6583d
lightlitebug <light.lite.bug@gmail.com>
2022-11-29 22:38:51 +0900

    Changing app theme according to current temperature

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
+import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'package:redux/redux.dart';
+import 'package:weather_redux/constants/constants.dart';
+
+import 'package:weather_redux/redux/app_middleware.dart';
 
 import 'pages/home_page.dart';
-import 'redux/app_middleware.dart';
 import 'redux/app_reducer.dart';
 import 'redux/app_state.dart';
 
@@ -27,14 +30,36 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return StoreProvider<AppState>(
       store: store,
-      child: MaterialApp(
-        title: 'Weather App',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: const HomePage(),
+      child: StoreConnector<AppState, _ViewModel>(
+        distinct: true,
+        converter: (Store<AppState> store) => _ViewModel.fromStore(store),
+        builder: (BuildContext context, _ViewModel vm) {
+          return MaterialApp(
+            title: 'Weather App',
+            debugShowCheckedModeBanner: false,
+            theme: vm.currentTemp > kWarmOrNot
+                ? ThemeData.light()
+                : ThemeData.dark(),
+            home: const HomePage(),
+          );
+        },
       ),
+    );
+  }
+}
+
+class _ViewModel extends Equatable {
+  final double currentTemp;
+  const _ViewModel({
+    required this.currentTemp,
+  });
+
+  @override
+  List<Object> get props => [currentTemp];
+
+  static fromStore(Store<AppState> store) {
+    return _ViewModel(
+      currentTemp: store.state.weatherState.weather.temp,
     );
   }
 }
