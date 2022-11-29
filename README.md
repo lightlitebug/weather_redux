7931ead5843b7069b8472a5410e389e7e7156702
lightlitebug <light.lite.bug@gmail.com>
2022-11-29 13:33:05 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# weather_redux
+
+### Weather App using Flutter Redux
+
+- data source: https://openweathermap.org/
---
+++ android/app/src/main/kotlin/com/example/weather_redux/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.weather_redux
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
